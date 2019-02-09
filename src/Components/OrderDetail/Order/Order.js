5d52d5cd8b265b3e2d8ea8eefb9b0953f3bac7d4
belek <belektolubaev@gmail.com>
2019-02-09 18:51:05 +0600

    Добавление тернарного оператора, который выводит блюдо по одному

--- src/Components/OrderDetail/Order/Order.js
+++ src/Components/OrderDetail/Order/Order.js
@@ -4,11 +4,13 @@ import { ListGroup, ListGroupItem, Button} from 'reactstrap';
 function Order(props) {
 
     return <ListGroup>
-            <ListGroupItem className='bg-dark text-warning'>
+        {props.count() > 0 ?
+            <ListGroupItem className='bg-dark text-warning mb-2'>
                 <span>{props.label} </span>
                 <span> x{props.count()} = {props.total()} сом</span>
                 <Button color='danger' disabled={props.isDisabled()} className="float-right" onClick={(event) => props.changeFood(event)}>X</Button>
             </ListGroupItem>
+            : null}
     </ListGroup>
 }
 
