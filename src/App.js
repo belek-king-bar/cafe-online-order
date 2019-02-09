9e2c5b0281f73019067e4db5bba8dfe26a9b92de
belek <belektolubaev@gmail.com>
2019-02-09 16:45:35 +0600

    Работа над App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './Components/Menu/Menu.js';
+import OrderDetail from './Components/OrderDetail/OrderDetail.js';
+import Food from './Components/Menu/Foods/Foods.js';
+import Order from './Components/OrderDetail/Order/Order.js';
+import {Container, Row, Col} from 'reactstrap';
+
+const availableFoods = [
+    {name: 'hamburger', price: 80, label: 'Гамбургер'},
+    {name: 'coffee', price: 70, label: 'Кофе'},
+    {name: 'cheeseburger', price: 90, label: 'Чизбургер'},
+    {name: 'tea', price: 50, label: 'Чай'},
+    {name: 'fries', price: 45, label:'Фри'},
+    {name: 'cola', price: 40, label:'Кола'}
+];
+
 
 class App extends Component {
+
+
+  state = {
+      foods: {
+          hamburger: {count: 0, total: 0},
+          coffee: {count: 0, total: 0},
+          cheeseburger: {count: 0, total: 0},
+          tea: {count: 0, total: 0},
+          fries: {count: 0, total: 0},
+          cola: {count: 0, total: 0}
+      }
+  };
+
+
+  getTotal = () => {
+        let total = 0;
+        let keys = Object.keys(this.state.foods);
+        let counter = 0;
+
+
+        while(counter < keys.length){
+            total += this.state.foods[keys[counter]].total;
+            counter++;
+        }
+        return total;
+
+
+    };
+
+  getCount = (name) => {
+    return this.state.foods[name].count;
+};
+
+  getFoodTotal = (name) => {
+    return this.state.foods[name].total;
+};
+
+  isAddButtonDisabled = (name) => {
+       return this.state.foods[name].count === 0;
+    };
+
+foodChanger = (name, event) => {
+
+        let food = {...this.state.foods[name]};
+        let price = availableFoods.find(item => item.name === name).price;
+        if (event.target.value === 'add') {
+            food.count += 1;
+        }else {
+            food.count -= 1;
+        }
+
+        food.total = food.count * price;
+
+        let foods = {...this.state.foods};
+        foods[name] = food;
+
+        let state = {...this.state};
+        state.foods = foods;
+
+        this.setState(state);
+
+    };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Container>
+            <Row>
+              <Col xs={5}>
+                  <OrderDetail total={this.getTotal()}>
+
+                    {availableFoods.map(item => {
+                        return <Order
+                            key={item.name}
+                            label={item.label}
+                            count={() => this.getCount(item.name)}
+                            total={() => this.getFoodTotal(item.name)}
+                            isDisabled={() => this.isAddButtonDisabled(item.name)}
+                            changeFood={(event) => this.foodChanger(item.name, event)}/>
+                    })}
+                  </OrderDetail>
+              </Col>
+              <Col xs={7}>
+                  <Menu>
+                      {availableFoods.map(item => {
+                          return <Food
+                          label={item.label}
+                          key={item.name}
+                          price={item.price}
+                          changeFood={(event) => this.foodChanger(item.name, event)}/>
+                          })}
+                  </Menu>
+              </Col>
+            </Row>
+          </Container>
       </div>
     );
   }
